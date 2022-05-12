9d426f7673a4acbeb98a056793293ca148d384d4
ekhrabroff <e.khrabroff@gmail.com>
2022-05-12 20:41:36 +0300

    Курсовая работа II вариант

--- Variant II.py
+++ Variant II.py
@@ -4,43 +4,41 @@
 #Задача.
 #Перепишите процедуру  MERGE_SORT и отсортируйте последовательность по возрастанию
 
-A = [31, 41, 9, 26, 41, 58, -1 , 6 , 101 , 13]
+import math
+mass = [31, 41, 9, 26, 41, 58, -1 , 6 , 101 , 13]
 
 def merge(A, p, q, r):
     n1 = q - p + 1
     n2 = r - q
-    L = [i for i in range(1, n1 + 1)]
-    R = [i for i in range(1, n2 + 1)]
-
-
+    L = [i for i in range(0, n1 +1)]
+    R = [i for i in range(0, n2 +1)]
+   
     for i in range(n1):
         L[i] = A[p + i - 1]
     for j in range(n2):
         R[j] = A[q + j]
-    L[n1 -1] = 2000
-    R[n2 -1] = 2000
-
+    L[n1] = math.inf
+    R[n2] = math.inf
+    
     i = 0
     j = 0
-    for k in range(p, r):
+    for k in range(p-1, r):
         if L[i] <= R[j]:
             A[k] = L[i]
             i += 1
         else:
             A[k] = R[j]
             j += 1
-    return A
 
-def merge_sort(A, p, r):
+
+def merge_sort(m, p, r):
     if p < r:
         q = (p + r) // 2
-        merge_sort(A, p, q)
-        merge_sort(A, q + 1, r)
-        merge(A, p, q, r)
-        return A
-
-print(len(A))
-B = merge_sort(A, 0, len(A) - 1)
+        merge_sort(m, p, q)
+        merge_sort(m, q + 1, r)
+        merge(m, p, q, r)
 
 
-print(B)
+merge_sort(mass, 1, len(mass))
+
+print(mass)
